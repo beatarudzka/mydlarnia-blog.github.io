3e00fd4dc359ea00de94e00d01dc37eafdc2cc56
Beata Rudzka <beatarudzka89@gmail.com>
2020-02-11 16:55:04 +0100

    styling article card

--- blog/src/components/BlogPost/index.js
+++ blog/src/components/BlogPost/index.js
@@ -5,9 +5,18 @@ import Card from '../UI/Card'
 
 const BlogPost = (props) => {
   return (
-    <Card style={{ width: '70%' }}>
-      BlogPost
-    </Card>
+    <div className="post__container">
+      <Card>
+        <div className="post__header">
+          <span className="post__header--category">Aromaterapia</span>
+          <h1 className="post__header--title">Aromaterapia – jak wybrać olejek aromatyczny dla siebie?</h1>
+          <span className="post_header--details"> 11 Luty, 2020, Beata Rudzka</span>
+        </div>
+        <div className="image_container">
+          < img className="image" src={require('../../blogPostImages/img6.jpg')} alt="Aromaterapy" />
+        </div>
+      </Card>
+    </div>
   )
 }
 
